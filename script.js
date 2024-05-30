48ba7f2fd019579524da5fea0f6a45ef63a2efdd
Farhan Chowdury <71190375+fc2078@users.noreply.github.com>
2024-05-30 14:51:06 -0400

    fix MAJOR functions issue + dynamics

--- script.js
+++ script.js
@@ -1,82 +1,74 @@
-// Rizz counter startup
-let counter = 0;
+// Counter setup
+let counter = 0
 let rizz = document.getElementById("rizz")
 rizz.innerText = "Your Rizz: 0"
 
-// The actual clicker
-function increaseRizz() {
-    counter = counter + 1
-    rizz.innerText = "Your Rizz: " + rizz
-}
+let rizzBoostCounter = 0
+let rizzBoost = document.getElementById("rizzBoostOwned")
+let rizzBoostCost = document.getElementById("rizzBoostCost")
+rizzBoost.innerText = "Amount Owned: 0"
 
-function gameLoop() {
-    //Wire Counter is used to increase click power.//
-    counter = counter + (20 * ramCounter) + (10 * gpuCounter) + (600 * hdCounter)
-    rizz.innerText = "Money: $" + counter
+// Clicker function
+function addRizz() {
+    counter = counter + 1 + rizzBoostCounter
+    rizz.innerText = "Your Rizz: " + counter
 }
 
 // Upgrades setup
-let rizzBoostAmount = 0;
-let rizzBoost = document.getElementById('rizzBoostAmt')
-let rizzBoostCost = document.getElementById("rizzBoostCost")
-rizzBoost.innerText = "Amount Owned: 0";
+let autorizzerCounter = 0;
+let autorizzer = document.getElementById('autorizzerAmt')
+let autorizzerCost = document.getElementById("autorizzerCost")
+autorizzer.innerText = "Amount Owned: 0";
 
-let autoRizzerAmount = 0;
-let autoRizzer = document.getElementById('autoRizzerAmt')
-let autoRizzerCost = document.getElementById("autoRizzerCost")
-autoRizzer.innerText = "Amount Owned: 0";
-
-let watermelonCatAmount = 0;
+let watermelonCatCounter = 0;
 let watermelonCat = document.getElementById('watermelonCatAmt')
 let watermelonCatCost = document.getElementById("watermelonCatCost")
 watermelonCat.innerText = "Amount Owned: 0";
 
-let enhancedInstagramAmount = 0;
+let enhancedInstagramCounter = 0;
 let enhancedInstagram = document.getElementById('enhancedInstagramAmt')
 let enhancedInstagramCost = document.getElementById("enhancedInstagramCost")
 enhancedInstagram.innerText = "Amount Owned: 0";
 
-let saulGoodmanAmount = 0;
+let saulGoodmanCounter = 0;
 let saulGoodman = document.getElementById('saulGoodmanAmt')
 let saulGoodmanCost = document.getElementById("saulGoodmanCost")
 saulGoodman.innerText = "Amount Owned: 0";
 
-let babyGronkAmount = 0;
+let babyGronkCounter = 0;
 let babyGronk = document.getElementById('babyGronkAmt')
 let babyGronkCost = document.getElementById("babyGronkCost")
 babyGronk.innerText = "Amount Owned: 0";
 
-let bugattiChironAmount = 0;
+let bugattiChironCounter = 0;
 let bugattiChiron = document.getElementById('bugattiChironAmt')
 let bugattiChironCost = document.getElementById("bugattiChironCost")
 bugattiChiron.innerText = "Amount Owned: 0";
 
-let airbusA380Amount = 0;
+let airbusA380Counter = 0;
 let airbusA380 = document.getElementById('airbusA380Amt')
 let airbusA380Cost = document.getElementById("airbusA380Cost")
 airbusA380.innerText = "Amount Owned: 0";
 
-
-setInterval(gameLoop, 1000)
-setInterval(checkPrice, 10);
-
 // Purchasing upgrades
-function buyRAM() {
-    let ramPrice = 700 + ramCounter ** 3
-    ramCost.innerText = "Cost: $" + ramPrice.toFixed(0)
-    if (counter >= ramPrice.toFixed(0)) {
-        counter = counter - ramPrice.toFixed(0)
-        rizzCounter.innerText = "Money: $" + counter
-        ramCounter = ramCounter + 1
-        ram.innerText = "Amount Owned: " + ramCounter
-        ramPrice = 700 + ramCounter ** 3
-        ramCost.innerText = "Cost: $" + ramPrice.toFixed(0)
+function buyRizzBoost() {
+    let rizzBoostPrice = 20 + rizzBoostCounter ** 1.5
+    rizzBoostCost.innerText = "Cost: " + rizzBoostPrice.toFixed(0) + " Rizz"
+    if (counter >= rizzBoostPrice.toFixed(0)) {
+        counter = counter - rizzBoostPrice.toFixed(0)
+        rizz.innerText = "Your Rizz: " + counter
+        rizzBoostCounter = rizzBoostCounter + 1
+        rizzBoost.innerText = "Amount Owned: " + rizzBoostCounter
+        rizzBoostPrice = 20 + rizzBoostCounter ** 1.5
+        rizzBoostCost.innerText = "Cost: " + rizzBoostPrice.toFixed(0) + " Rizz"
     }
     else {
-        let moneyRAMLeft = ramPrice.toFixed(0) - counter
-        alert(`Not enough rizz! You are very beta and broke. Achieve ${totalPrice} rizz to buy this upgrade so KEEP THE RIZZ UP AND STRIVE TO BECOME A SIGMA`);
+        document.getElementById("rizzBoost").disabled = true;
+        let rizzRizzBoostLeft = rizzBoostPrice.toFixed(0) - counter
+        alert("Not enough rizz! You are very beta and broke. You need " + rizzRizzBoostLeft + " more rizz to buy this upgrade SO STRIVE TO BECOME A SIGMA AND KEEP RIZZIN'!")
     }
 }
+
 function buyItem(upgrade) {
     let quantity = buyMultiplier;
     let totalPrice = upgrades[upgrade].price * quantity;
@@ -92,6 +84,12 @@ function buyItem(upgrade) {
     }
 }
 
-// Initialize the default multiplier button as active and update prices
-updateMultiplierButtons();
-updatePrices();
+// Loop the game and apply the upgrades
+function gameLoop() {
+    counter = counter + (5 * rizzBoostAmount) + (10 * autorizzerAmount) + (20 * watermelonCatAmount) + (35 * enhancedInstagramAmount) + (50 * saulGoodmanAmount) + (100 * babyGronkAmount) + (200 * bugattiChironAmount) + (500 * airbusA380Amount)
+    rizz.innerText = "Your Rizz: " + counter
+}
+
+// Intervals
+setInterval(gameLoop, 1000)
+setInterval(checkPrice, 10);
