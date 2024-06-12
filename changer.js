93705dd829ab32a2bd442a31624df8d550584e7f
Farhan Chowdury <fchowdury@brooklynsteamcenter.org>
2024-06-12 19:30:02 -0400

    minor changes

--- changer.js
+++
@@ -1,4 +0,0 @@
-let something1Counter = 0;
-let something1 = document.getElementById('something1Owned')
-let something1Cost = document.getElementById("something1Cost")
-something1.innerText = "Amount Owned: 0";
